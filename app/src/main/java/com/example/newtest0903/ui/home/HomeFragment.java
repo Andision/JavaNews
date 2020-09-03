3e3eb29b0202059c5405cb43908e65c57e7c67ce
Andision <Andision@users.noreply.github.com>
2020-09-04 00:06:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/newtest0903/ui/home/HomeFragment.java
@@ -0,0 +1,72 @@
+package com.example.newtest0903.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.newtest0903.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    private List<Fragment> fragmentList = new ArrayList<Fragment>();
+    private String[] strings = new String[]{"A","B","C","D"};
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        fragmentList.add(new ListFragment());
+        fragmentList.add(new ListFragment());
+        fragmentList.add(new ListFragment());
+        fragmentList.add(new ListFragment());
+
+        TabLayout tab_layout = root.findViewById(R.id.tab_layout);
+        ViewPager viewPager = root.findViewById(R.id.vp);
+        MyAdapter fragmentAdapter = new  MyAdapter(getChildFragmentManager());
+        viewPager.setAdapter(fragmentAdapter);
+        tab_layout.setupWithViewPager(viewPager);
+
+
+        return root;
+    }
+
+    public class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return strings[position];
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/newtest0903/ui/home/ListFragment.java
@@ -0,0 +1,51 @@
+package com.example.newtest0903.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.newtest0903.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+    private String mFrom;
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+    public static Fragment newInstance(String from){
+        HomeFragment homeFragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("from",from);
+        homeFragment.setArguments(bundle);
+        return homeFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null){
+            mFrom = (String) getArguments().get("from");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        return view;
+    }
+
+}
+
+
