6b1227ea4ed7163345f33cf4bf05499476a8cddf
Andision <Andision@users.noreply.github.com>
2020-09-05 16:18:15 +0800

    Add TabLayout+ViewPager in Notification

--- app/src/main/java/com/example/newtest0903/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/newtest0903/ui/notifications/NotificationsFragment.java
@@ -1,35 +1,82 @@
 package com.example.newtest0903.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.newtest0903.R;
+import com.example.newtest0903.news.NewsActivity;
+import com.example.newtest0903.ui.home.ListFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private List<Fragment> fragmentList = new ArrayList<Fragment>();
+    private String[] strings = new String[]{"A","B","C","D"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        //set viewpager---------------------------------------------
+        fragmentList.add(new ListFragment());
+        fragmentList.add(new ListFragment());
+        fragmentList.add(new ListFragment());
+        fragmentList.add(new ListFragment());
+
+        TabLayout tab_layout = root.findViewById(R.id.tab_layout);
+        ViewPager viewPager = root.findViewById(R.id.vp);
+        MyFragmentAdapter fragmentAdapter = new MyFragmentAdapter(getChildFragmentManager());
+        viewPager.setAdapter(fragmentAdapter);
+        tab_layout.setupWithViewPager(viewPager);
+        //set viewpager*********************************************
+
+
+
         return root;
     }
+
+
+    public class MyFragmentAdapter extends FragmentPagerAdapter {
+        public MyFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return strings[position];
+        }
+    }
+
+
+
 }
